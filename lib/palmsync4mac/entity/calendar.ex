c120fdf67131f5c4f7ccfbcff89e62d513577545
Stefan Wendler <stefan.wendler@tngtech.com>
2024-11-02 18:20:05 +0100

    2: Modify calendar entity to support multiple sources and destinations

--- lib/palmsync4mac/entity/calendar.ex
+++ lib/palmsync4mac/entity/calendar.ex
@@ -15,36 +15,61 @@ defmodule PalmSync4Mac.Entity.Calendar do
     defaults([:read, :destroy])
 
     create(:create) do
-      accept([:title, :start_date, :end_date])
+      accept([:source, :title, :start_date, :end_date, :description, :location, :deleted])
     end
   end
 
   attributes do
     uuid_primary_key(:id)
 
+    attribute(:source, :string) do
+      description("The source of the event. Can be one of :apple, <palm device id>")
+      allow_nil?(false)
+      public?(true)
+    end
+
     attribute(:title, :string) do
+      description("The title of the event")
       allow_nil?(false)
       public?(true)
     end
 
-    attribute(:start_date, :integer) do
+    attribute(:start_date, :utc_datetime) do
+      description("The start date of the event in UTC")
       allow_nil?(false)
       public?(true)
     end
 
-    attribute(:end_date, :integer) do
+    attribute(:end_date, :utc_datetime) do
+      description("The end date of the event in UTC")
       allow_nil?(false)
       public?(true)
     end
 
-    attribute(:all_day, :boolean) do
+    attribute(:description, :string) do
+      description("The description of the event, e.g. what it is about")
       allow_nil?(true)
       public?(true)
     end
 
-    attribute(:status, :atom) do
-      description("Status of the event. Can be one of :new, :updated, :deleted")
-      allow_nil?(false)
+    attribute(:location, :string) do
+      description("The location of the event, e.g. address, zoom link, etc")
+      allow_nil?(true)
+      public?(true)
+    end
+
+    attribute(:last_modified, :utc_datetime) do
+      description("The last time the event was modified")
+      allow_nil?(true)
+      public?(true)
+    end
+
+    attribute(:deleted, :boolean) do
+      description(
+        "Whether the event has been deleted. It's a soft delete, i.e. the record still exists in the database but is not shown and will be synced as a deleted event"
+      )
+
+      default(false)
       public?(true)
     end
   end
