1e042244ca8b2fe152cc7ea632fcf71746eb9218
Stefan W <bsu@milliways.local>
2024-12-30 11:36:53 +0100

    backup: start definint palm types and attributes

---
+++ lib/palmsync4mac/palm_structures.ex
@@ -0,0 +1,12 @@
+defmodule PalmSync4Mac.PalmStructures do
+  @moduledoc """
+  The PalmSync4Mac Domain representing the PDB/PRC structures
+  """
+  use Ash.Domain
+
+  resources do
+    resource(PalmSync4Mac.PalmStructures.PdbHeader)
+    resource(PalmSync4Mac.PalmStructures.DateBookEntry)
+    resource(PalmSync4Mac.PalmStructures.PdbAttributes)
+  end
+end
