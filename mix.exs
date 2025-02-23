f37e8e6828e4ec9258e7f67f9437c271c3e2b72c
Stefan W <bsu@milliways.universe>
2025-02-23 18:26:13 +0100

    elixir 1.18

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PalmSync4Mac.MixProject do
     [
       app: :palmsync4mac,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
