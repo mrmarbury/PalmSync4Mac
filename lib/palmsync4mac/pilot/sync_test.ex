ed70cf0ba78afd361f08277013e6f235a66572d3
Stefan Wendler <stefan.wendler@tngtech.com>
2025-11-07 23:13:57 +0100

    add time sync

--- lib/palmsync4mac/pilot/sync_test.ex
+++ lib/palmsync4mac/pilot/sync_test.ex
@@ -5,6 +5,7 @@ defmodule PalmSync4Mac.Pilot.SyncTest do
   # FIXME:#12 Remove this when the UI is ready and we have an integration test that can take this over
   def sync do
     pre_queue = [
+      {PalmSync4Mac.Pilot.SyncWorker.MiscWorker, :time_sync, []},
       {PalmSync4Mac.Pilot.SyncWorker.UserInfoWorker, :pre_sync, ["PalmTX"]}
     ]
 
@@ -13,7 +14,6 @@ defmodule PalmSync4Mac.Pilot.SyncTest do
     ]
 
     psr = %PalmSync4Mac.Pilot.SyncWorker.MainWorker.PilotSyncRequest{
-      terminate_after_sync: false,
       pre_sync_queue: pre_queue,
       post_sync_queue: post_queue
     }
---
+++ lib/palmsync4mac/pilot/sync_worker/misc_worker.ex
@@ -0,0 +1,39 @@
+defmodule PalmSync4Mac.Pilot.SyncWorker.MiscWorker do
+  @moduledoc """
+  Sync Worker for all the sync functions that do not really fit anywhere.
+
+  It's currently a bit overkill to have a GenServer for such simple calls
+  that do not really need state. And Erik would hate this for sure. 
+  But it fits the current sync model well.
+  """
+  use GenServer
+
+  require Logger
+
+  alias PalmSync4Mac.Comms.Pidlp
+
+  defstruct client_sd: -1
+
+  def start_link(worker_info \\ %__MODULE__{}) do
+    GenServer.start_link(__MODULE__, worker_info, name: __MODULE__)
+  end
+
+  def time_sync do
+    GenServer.call(__MODULE__, :time_sync)
+  end
+
+  @impl true
+  def init(worker_info) do
+    Logger.info("Started #{__MODULE__} for #{worker_info.client_sd}")
+    {:ok, worker_info}
+  end
+
+  def handle_call(:time_sync, _from, state) do
+    timestamp = DateTime.utc_now() |> DateTime.to_unix()
+
+    case Pidlp.set_sys_date_time(state.client_sd, timestamp) do
+      {:ok, _client_sd} -> {:reply, :ok, state}
+      {:error, message} -> {:reply, {:error, message}, state}
+    end
+  end
+end
